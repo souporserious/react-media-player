d700b87260112e3774a6908a582c53ebf43c25a4
Travis Arnold <ftntravis@gmail.com>
2016-02-28 13:23:46 -0800

    better vimeo id regex

--- src/utils/get-vimeo-id.js
+++ src/utils/get-vimeo-id.js
@@ -1,9 +1,10 @@
+// Thanks to: http://stackoverflow.com/a/13286930/1461204
 export default function getVimeoId(url) {
-  const regExp = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/
+  const regExp = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|video\/|)(\d+)(?:$|\/|\?)/
   const match = url.match(regExp)
 
   if (match) {
-    return match[5]
+    return match[3]
   } else {
     throw 'Invalid Vimeo ID provided'
   }
