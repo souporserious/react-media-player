75542c8a579cc92898bb193a4ea134b86ea4e4a7
Travis Arnold <ftntravis@gmail.com>
2016-07-09 18:19:26 -0700

    remove get file extension in favor of regex

--- src/utils/get-vendor.js
+++ src/utils/get-vendor.js
@@ -1,21 +1,18 @@
-import getFileExtension from './get-file-extension'
 import Youtube from '../vendors/Youtube'
 import Vimeo from '../vendors/Vimeo'
 import HTML5 from '../vendors/HTML5'
 
-const VIDEO_EXTENSIONS = ['mp4', 'm4v', 'webm', 'ogv']
-const AUDIO_EXTENSIONS = ['mp3', 'm4a', 'wav', 'ogg']
+const VIDEO_EXTENSIONS = /\.(mp4|og[gv]|webm|mov|m4v)($|\?)/i
+const AUDIO_EXTENSIONS = /\.(mp3|wav|m4a)($|\?)/i
 
 export default function getVendor(src, vendor) {
-  const ext = getFileExtension(src)
-
   if (vendor === 'youtube' || src.indexOf('youtube.com') > -1 || src.indexOf('youtu.be') > -1) {
     return { vendor: 'youtube', component: Youtube }
   } else if (vendor === 'vimeo' || src.indexOf('vimeo.com') > -1) {
     return { vendor: 'vimeo', component: Vimeo }
-  } else if (vendor === 'video' || VIDEO_EXTENSIONS.indexOf(ext) > -1) {
+  } else if (vendor === 'video' || VIDEO_EXTENSIONS.test(src)) {
     return { vendor: 'video', component: HTML5 }
-  } else if (vendor === 'audio' || AUDIO_EXTENSIONS.indexOf(ext) > -1) {
+  } else if (vendor === 'audio' || AUDIO_EXTENSIONS.test(src)) {
     return { vendor: 'audio', component: HTML5 }
   } else {
     console.warn('Warning: Player was not rendered. Source could not be determined.')
