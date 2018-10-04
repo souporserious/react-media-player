2a19cb017f47d3a31f46bd2eacca0e261054add9
Travis Arnold <ftntravis@gmail.com>
2018-10-04 14:13:09 -0700

    cleanup example

--- example/index.jsx
+++ example/index.jsx
@@ -7,26 +7,12 @@ import AudioPlayer from './AudioPlayer'
 import CirclePlayer from './CirclePlayer'
 
 import { Media, Player, controls } from '../src/react-media-player'
-const { PlayPause } = controls
+import playlist from './playlist'
 
 import './main.scss'
 
+const { PlayPause } = controls
 const mod = (num, max) => ((num % max) + max) % max
-const playlist = [
-  { src: 'http://www.youtube.com/embed/h3YVKTxTOgU', label: 'Brand New (Youtube)' },
-  { src: 'https://youtu.be/VOyYwzkQB98', label: 'Neck Deep (Youtube)' },
-  { src: 'https://player.vimeo.com/video/156147818', label: 'Pump (Vimeo)' },
-  { src: 'https://vimeo.com/channels/staffpicks/150734165', label: 'Lesley (Vimeo)' },
-  { src: 'http://a1083.phobos.apple.com/us/r1000/014/Music/v4/4e/44/b7/4e44b7dc-aaa2-c63b-fb38-88e1635b5b29/mzaf_1844128138535731917.plus.aac.p.m4a', label: 'iTunes Preview' },
-  { src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4', label: 'Big Buck Bunny' },
-  { src: 'https://vid4u.org/ninja/5/dev/assets/madmax-intro.mp4', label: 'Mad Max Intro' },
-  { src: 'http://demosthenes.info/assets/videos/mountain.mp4', label: 'Mountain' },
-  { src: 'http://www.w3schools.com/html/movie.mp4', label: 'Bear' },
-  { src: 'http://jelmerdemaat.nl/online-demos/conexus/video/small.mp4', label: 'Lego Robot' },
-  { src: 'http://shapeshed.com/examples/HTML5-video-element/video/320x240.m4v', label: 'iPod Help' },
-  { src: 'http://html5demos.com/assets/dizzy.mp4', label: 'Dizzy Kitty' },
-  { src: 'http://www.noiseaddicts.com/samples_1w72b820/3890.mp3', label: 'Noise Addicts' }
-]
 
 class Playlist extends Component {
   _handleTrackClick(track) {
@@ -41,15 +27,17 @@ class Playlist extends Component {
           <h3 className="media-playlist-title">Playlist</h3>
         </header>
         <ul className="media-playlist-tracks">
-          {tracks.map(track =>
+          {tracks.map(track => (
             <li
               key={track.label}
-              className={`media-playlist-track ${track === currentTrack ? 'is-active' : ''}`}
+              className={`media-playlist-track ${
+                track === currentTrack ? 'is-active' : ''
+              }`}
               onClick={this._handleTrackClick.bind(this, track)}
             >
               {track.label}
             </li>
-          )}
+          ))}
         </ul>
       </aside>
     )
@@ -61,15 +49,18 @@ class App extends Component {
     currentTrack: { src: null, label: 'No media loaded' },
     showMediaPlayer: true,
     repeatTrack: false,
-    autoPlay: true
+    autoPlay: true,
   }
 
-  _handleTrackClick = (track) => {
+  _handleTrackClick = track => {
     this.setState({ currentTrack: track })
   }
 
-  _navigatePlaylist = (direction) => {
-    const newIndex = mod(playlist.indexOf(this.state.currentTrack) + direction, playlist.length)
+  _navigatePlaylist = direction => {
+    const newIndex = mod(
+      playlist.indexOf(this.state.currentTrack) + direction,
+      playlist.length
+    )
     this.setState({ currentTrack: playlist[newIndex] })
   }
 
@@ -82,10 +73,10 @@ class App extends Component {
         >
           Toggle Media Player
         </button>
-        { showMediaPlayer &&
+        {showMediaPlayer && (
           <div className="media-player-wrapper">
             <MediaPlayer
-              ref={c => this._mediaPlayer = c}
+              ref={c => (this._mediaPlayer = c)}
               src={currentTrack.src}
               autoPlay={autoPlay}
               loop={repeatTrack}
@@ -93,7 +84,9 @@ class App extends Component {
               repeatTrack={repeatTrack}
               onPrevTrack={() => this._navigatePlaylist(-1)}
               onNextTrack={() => this._navigatePlaylist(1)}
-              onRepeatTrack={() => { this.setState({ repeatTrack: !repeatTrack }) }}
+              onRepeatTrack={() => {
+                this.setState({ repeatTrack: !repeatTrack })
+              }}
               onPlay={() => !autoPlay && this.setState({ autoPlay: true })}
               onPause={() => this.setState({ autoPlay: false })}
               onEnded={() => !repeatTrack && this._navigatePlaylist(1)}
@@ -104,10 +97,10 @@ class App extends Component {
               onTrackClick={this._handleTrackClick}
             />
           </div>
-        }
-        <VideoPlayer src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"/>
-        <AudioPlayer src="/audio/armstrong.mp3"/>
-        <CirclePlayer src="https://p.scdn.co/mp3-preview/f83458d6611ae9589420f71c447ac9d2e3047cb8"/>
+        )}
+        <VideoPlayer src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" />
+        <AudioPlayer src="/audio/armstrong.mp3" />
+        <CirclePlayer src="https://p.scdn.co/mp3-preview/f83458d6611ae9589420f71c447ac9d2e3047cb8" />
       </div>
     )
   }
