e18d7c381bda7f02b67a6fbdb4a8b20d8cacdb6e
Gregory Shilin <gshilin@gmail.com>
2019-08-11 14:23:00 +0300

    upgrade to 0.7.7

--- src/Media.jsx
+++ src/Media.jsx
@@ -1,12 +1,11 @@
-import React, { Component, Children } from 'react'
-import PropTypes from 'prop-types'
-import ReactDOM, { findDOMNode } from 'react-dom'
-import contextTypes from './context-types'
-import requestFullscreen from './utils/request-fullscreen'
-import exitFullscreen from './utils/exit-fullscreen'
-import fullscreenChange from './utils/fullscreen-change'
+import React, { Children, Component } from 'react';
+import PropTypes from 'prop-types';
+import contextTypes from './context-types';
+import requestFullscreen from './utils/request-fullscreen';
+import exitFullscreen from './utils/exit-fullscreen';
+import fullscreenChange from './utils/fullscreen-change';
 
-const MEDIA_EVENTS = {
+const MEDIA_EVENTS      = {
   onPlay: 'isPlaying',
   onPause: 'isPlaying',
   onDuration: 'duration',
@@ -15,15 +14,15 @@ const MEDIA_EVENTS = {
   onMute: 'isMuted',
   onVolumeChange: 'volume',
   onError: null,
-}
-const MEDIA_EVENTS_KEYS = Object.keys(MEDIA_EVENTS)
+};
+const MEDIA_EVENTS_KEYS = Object.keys(MEDIA_EVENTS);
 
 class Media extends Component {
   static propTypes = {
     children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]).isRequired,
-  }
+  };
 
-  static childContextTypes = contextTypes
+  static childContextTypes = contextTypes;
 
   state = {
     currentTime: 0,
@@ -34,11 +33,11 @@ class Media extends Component {
     isPlaying: false,
     isMuted: false,
     isFullscreen: false,
-  }
+  };
 
-  _isMounted = false
-  _playerProps = {}
-  _lastVolume = 0
+  _isMounted   = false;
+  _playerProps = {};
+  _lastVolume  = 0;
 
   getChildContext() {
     return {
@@ -51,17 +50,17 @@ class Media extends Component {
       _mediaGetters: {
         getPlayerEvents: this._getPlayerEvents(),
       },
-    }
+    };
   }
 
   componentDidMount() {
-    this._isMounted = true
-    fullscreenChange('add', this._handleFullscreenChange)
+    this._isMounted = true;
+    fullscreenChange('add', this._handleFullscreenChange);
   }
 
   componentWillUnmount() {
-    this._isMounted = false
-    fullscreenChange('remove', this._handleFullscreenChange)
+    this._isMounted = false;
+    fullscreenChange('remove', this._handleFullscreenChange);
   }
 
   _getPublicMediaProps() {
@@ -78,136 +77,136 @@ class Media extends Component {
       setVolume: this.setVolume,
       addVolume: this.addVolume,
       fullscreen: this.fullscreen,
-    }
+    };
   }
 
   _getPlayerEvents() {
-    const events = {}
+    const events = {};
     MEDIA_EVENTS_KEYS.forEach(key => {
-      const stateKey = MEDIA_EVENTS[key]
+      const stateKey           = MEDIA_EVENTS[key];
       const handlePropCallback = () => {
-        const propCallback = this._playerProps[key]
+        const propCallback = this._playerProps[key];
         if (typeof propCallback === 'function') {
-          propCallback(this.state)
+          propCallback(this.state);
         }
-      }
-      events[key] = val => {
+      };
+      events[key]              = val => {
         if (stateKey) {
           if (this._isMounted) {
-            this.setState({ [stateKey]: val }, handlePropCallback)
+            this.setState({ [stateKey]: val }, handlePropCallback);
           }
         } else {
-          handlePropCallback()
+          handlePropCallback();
         }
-      }
-    })
-    return events
+      };
+    });
+    return events;
   }
 
   _setPlayer = component => {
-    this._player = component
-  }
+    this._player = component;
+  };
 
   _setPlayerProps = props => {
-    this._playerProps = props
-  }
+    this._playerProps = props;
+  };
 
   _setPlayerState = state => {
-    this.setState(state)
-  }
+    this.setState(state);
+  };
 
   play = () => {
-    return this._player.play()
-  }
+    return this._player.play();
+  };
 
   pause = () => {
-    this._player.pause()
-  }
+    this._player.pause();
+  };
 
   playPause = () => {
     if (!this.state.isPlaying) {
-      return this.play()
+      return this.play();
     } else {
-      this.pause()
+      this.pause();
     }
-  }
+  };
 
   stop = () => {
-    this._player.stop()
-  }
+    this._player.stop();
+  };
 
   seekTo = currentTime => {
-    this._player.seekTo(currentTime)
-    this.setState({ currentTime })
-  }
+    this._player.seekTo(currentTime);
+    this.setState({ currentTime });
+  };
 
   skipTime = amount => {
-    const { currentTime, duration } = this.state
-    let newTime = currentTime + amount
+    const { currentTime, duration } = this.state;
+    let newTime                     = currentTime + amount;
     if (newTime < 0) {
-      newTime = 0
+      newTime = 0;
     } else if (newTime > duration) {
-      newTime = duration
+      newTime = duration;
     }
-    this.seekTo(newTime)
-  }
+    this.seekTo(newTime);
+  };
 
   mute = isMuted => {
     if (isMuted) {
-      this._lastVolume = this.state.volume
-      this._player.setVolume(0)
+      this._lastVolume = this.state.volume;
+      this._player.setVolume(0);
     } else {
-      const volume = this._lastVolume > 0 ? this._lastVolume : 0.1
-      this._player.setVolume(volume)
+      const volume = this._lastVolume > 0 ? this._lastVolume : 0.1;
+      this._player.setVolume(volume);
     }
-    this._player.mute(isMuted)
-  }
+    this._player.mute(isMuted);
+  };
 
   muteUnmute = () => {
-    this.mute(!this.state.isMuted)
-  }
+    this.mute(!this.state.isMuted);
+  };
 
   setVolume = volume => {
-    const isMuted = volume <= 0
+    const isMuted = volume <= 0;
     if (isMuted !== this.state.isMuted) {
-      this.mute(isMuted)
+      this.mute(isMuted);
     } else {
-      this._lastVolume = volume
+      this._lastVolume = volume;
     }
-    this._player.setVolume(volume)
-  }
+    this._player.setVolume(volume);
+  };
 
   addVolume = amount => {
-    let newVolume = this.state.volume + amount * 0.01
+    let newVolume = this.state.volume + amount * 0.01;
     if (newVolume < 0) {
-      newVolume = 0
+      newVolume = 0;
     } else if (newVolume > 1) {
-      newVolume = 1
+      newVolume = 1;
     }
-    this.setVolume(newVolume)
-  }
+    this.setVolume(newVolume);
+  };
 
   fullscreen = () => {
     if (!this.state.isFullscreen) {
-      this._player.node[requestFullscreen]()
+      this._player.node[requestFullscreen]();
     } else {
-      document[exitFullscreen]()
+      document[exitFullscreen]();
     }
-  }
+  };
 
   _handleFullscreenChange = ({ target }) => {
     if (target === this._player.node) {
-      this.setState({ isFullscreen: !this.state.isFullscreen })
+      this.setState({ isFullscreen: !this.state.isFullscreen });
     }
-  }
+  };
 
   render() {
-    const { children } = this.props
+    const { children } = this.props;
     if (typeof children === 'function') {
-      return children(this._getPublicMediaProps())
+      return children(this._getPublicMediaProps());
     }
-    return Children.only(children)
+    return Children.only(children);
   }
 }
 
-export default Media
+export default Media;
--- src/decorators/with-media-props.jsx
+++ src/decorators/with-media-props.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import contextTypes from '../context-types'
+import React, { Component } from 'react';
+import contextTypes from '../context-types';
 
 export default function withMediaProps(MediaComponent) {
   return class extends Component {
-    static displayName = 'withMediaProps'
+    static displayName = 'withMediaProps';
 
-    static contextTypes = contextTypes
+    static contextTypes = contextTypes;
 
     render() {
-      return <MediaComponent {...this.props} media={this.context.media} />
+      return <MediaComponent {...this.props} media={this.context.media} />;
     }
-  }
+  };
 }
