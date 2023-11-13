cd6c028e687986e5d6dc8da2fc0127328f95a2c1
Gregory Shilin <gshilin@gmail.com>
2023-11-13 04:57:36 +0200

    Update version of packages to the latest ones

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,49 @@
 var path = require("path");
 var webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 var TARGET = process.env.TARGET || null;
 
 var config = {
-  entry: {},
-  output: {
-    path: path.join(__dirname, "example"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      { test: /\.(js|jsx)/, exclude: /node_modules/, use: "babel-loader" },
-      { test: /\.(css|scss)/, use: "style!css!postcss!sass?sourceMap" }
-    ]
-  },
-  resolve: {
-    extensions: ["", ".js", ".jsx"]
-  },
-  plugins: [],
-  devServer: {
-    contentBase: "./example",
-    inline: true
-  }
+    entry: {},
+    output: {
+        path: path.join(__dirname, "example"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {test: /\.(js|jsx)/, exclude: /node_modules/, use: "babel-loader"},
+            {test: /\.(css|scss)/, use: "style!css!postcss!sass?sourceMap"}
+        ]
+    },
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
+    },
+    plugins: [],
+    devServer: {
+        contentBase: "./example",
+        inline: true
+    }
 };
 
 if (TARGET === "minify") {
-  config.entry.index = path.join(__dirname, "example/index.jsx");
+    config.entry.index = path.join(__dirname, "example/index.jsx");
 
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    })
-  );
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        })
+    );
 
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true,
-        warnings: false
-      },
-      mangle: {
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-        screw_ie8: true
-      }
-    })
-  );
+    config.plugins.push(
+        new webpack.optimize.TerserPlugin()
+    );
 } else {
-  config.entry.index = [
-    "webpack/hot/dev-server",
-    path.join(__dirname, "example/index.jsx")
-  ];
+    config.entry.index = [
+        "webpack/hot/dev-server",
+        path.join(__dirname, "example/index.jsx")
+    ];
 }
 
 module.exports = config;
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,62 +1,57 @@
-var path    = require('path');
+var path = require('path');
 var webpack = require('webpack');
-var banner  = require('./webpack.banner');
-var TARGET  = process.env.TARGET || null;
+var banner = require('./webpack.banner');
+var TARGET = process.env.TARGET || null;
 
 var externals = {
-  react: {
-    root: 'React',
-    commonjs2: 'react',
-    commonjs: 'react',
-    amd: 'react'
-  },
-  'react-dom': {
-    root: 'ReactDOM',
-    commonjs2: 'react-dom',
-    commonjs: 'react-dom',
-    amd: 'react-dom'
-  }
+    react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+    },
+    'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+    }
 };
 
 var config = {
-  mode: 'production',
-  entry: {
-    index: './src/react-media-player.js'
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: 'dist/',
-    filename: 'react-media-player.js',
-    sourceMapFilename: 'react-media-player.sourcemap.js',
-    library: 'ReactMediaPlayer',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [{ test: /\.(js|jsx)/, use: 'babel-loader' }]
-  },
-  plugins: [new webpack.BannerPlugin(banner)],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  externals: externals
+    mode: 'production',
+    entry: {
+        index: './src/react-media-player.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: 'dist/',
+        filename: 'react-media-player.js',
+        sourceMapFilename: 'react-media-player.sourcemap.js',
+        library: 'ReactMediaPlayer',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [{test: /\.(js|jsx)/, use: 'babel-loader'}]
+    },
+    plugins: [new webpack.BannerPlugin(banner)],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    externals: externals
 };
 
 if (TARGET === 'minify') {
-  const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+    const TerserPlugin = require("terser-webpack-plugin");
 
-  config.output.filename          = 'react-media-player.min.js';
-  config.output.sourceMapFilename = 'react-media-player.min.js';
-  config.optimization             = {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: false,
-          mangle: true,
-          ecma: 6,
-        },
-      })
-    ]
-  };
+    config.output.filename = 'react-media-player.min.js';
+    config.output.sourceMapFilename = 'react-media-player.min.js';
+    config.optimization = {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin()
+        ]
+    };
 }
 
 module.exports = config;
